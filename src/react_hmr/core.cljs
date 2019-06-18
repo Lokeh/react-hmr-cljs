b75276be1caf201cbb687d256bca908944386e2d
Will Acton <will@willacton.com>
2019-06-18 09:44:21 -0700

    init

---
+++ src/react_hmr/core.cljs
@@ -0,0 +1,53 @@
+(ns react-hmr.core
+  (:require [devcards.core :as dc :include-macros true]
+            [hiccup-next.react :refer [hiccup-element]]
+            ["react" :as react]
+            [cljs-bean.core :refer [bean]]))
+
+
+;; Restart Devcards on each hot load
+(defn ^:dev/after-load ^:export start []
+  (dc/start-devcard-ui!))
+
+;; Make printing React elements work
+(when (exists? js/Symbol)
+  (extend-protocol IPrintWithWriter
+    js/Symbol
+    (-pr-writer [sym writer _]
+      (-write writer (str "\"" (.toString sym) "\"")))))
+
+
+
+
+;;
+;; Example component
+;;
+
+(defn MyFirstComponent [props]
+  ;; wrap props in a `bean` to access and destructure like a Clojure map
+  (let [{:keys [name]} (bean props)] 
+    ;; return hiccup, which is like JSX - gets processed into React elements
+    #h/n [:div "Hello, " name]))
+
+
+;; render in the devcards UI
+(dc/defcard my-first-component
+  #h/n [MyFirstComponent {:name "joe"}])
+
+
+
+;;
+;; Hooks
+;;
+
+(defn MyStatefulComponent [props]
+  (let [[name set-name] (react/useState "jane")]
+    #h/n [:div
+          [:div "Hello, " name]
+          [:div [:input {:type "text"
+                         :value name
+                         :on-change #(set-name (.. % -target -value))}]]]))
+
+
+(dc/defcard my-stateful-component
+  #h/n [MyStatefulComponent])
